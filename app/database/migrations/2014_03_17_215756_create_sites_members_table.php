41f885b4ce582c31f1d43f3db2066a07367befa6
See Guo Lin <see@guol.in>
2014-03-18 06:03:19 +0800

    Add migration to create the `sites_members` table.

---
+++ app/database/migrations/2014_03_17_215756_create_sites_members_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSitesMembersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('sites_members', function($table)
+		{
+			$table->smallInteger('site_id')->unsigned();
+			$table->smallInteger('member_id')->unsigned();
+			$table->enum('access', array('view','manage_actions','manage_site','manage_account'))->default('view');
+
+			$table->primary(array('site_id', 'member_id'));
+
+			$table->foreign('site_id')->references('id')->on('sites')->onDelete('restrict')->onUpdate('cascade');
+			$table->foreign('member_id')->references('id')->on('members')->onDelete('restrict')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('sites_members');
+	}
+
+}
