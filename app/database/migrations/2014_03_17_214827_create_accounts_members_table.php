1afdf4423c6a012f0a9f551a707931d01111a08e
See Guo Lin <see@guol.in>
2014-03-18 05:52:09 +0800

    Add migration to create the `accounts_members` table.

---
+++ app/database/migrations/2014_03_17_214827_create_accounts_members_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccountsMembersTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('accounts_members', function($table)
+		{
+			$table->smallInteger('account_id')->unsigned();
+			$table->smallInteger('member_id')->unsigned();
+
+			$table->primary(array('account_id', 'member_id'));
+
+			$table->foreign('account_id')->references('id')->on('accounts')->onDelete('restrict')->onUpdate('cascade');
+			$table->foreign('member_id')->references('id')->on('members')->onDelete('restrict')->onUpdate('cascade');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('accounts_members');
+	}
+
+}
