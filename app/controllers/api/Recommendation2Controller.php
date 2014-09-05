7ede74615e1a526a6a81b7852842461290ea6559
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-09-05 15:28:01 +0800

    add ability of pull recommendation from tapirus. WIP of filtering.

--- app/controllers/api/Recommendation2Controller.php
+++ app/controllers/api/Recommendation2Controller.php
@@ -10,370 +10,106 @@
 
 namespace App\Controllers\Api;
 
-class Recommendation2Controller extends \App\Controllers\ApiBaseController
+use App\Controllers\ApiBaseController,
+    App\Pongo\Repository\RecommendationRepository,
+    Event,
+    Gui,
+    Input,
+    Response;
+
+class Recommendation2Controller extends ApiBaseController
 {
 
-	private $curl				 = null;
-	private $widget_id			 = null;
-	private $operator_types		 = array();
-	private $number_of_results	 = 10;
+    protected $response     = array();
+    private $operator_types = array();
+    protected $repository;
 
-	function __construct()
-	{
-		parent::__construct();
-		$this->operator_types = array(
-			'contain'			 => 'Contains',
-			'equal'				 => 'Equals',
-			'not_equal'			 => 'Not Equals',
-			'is_set'			 => 'Is Set',
-			'is_not_set'		 => 'Is Not Set',
-			'greater_than'		 => 'Greater Than',
-			'greater_than_equal' => 'Greater Than or Equal',
-			'less_than'			 => 'Less Than',
-			'less_than_equal'	 => 'Less Than or Equal'
-		);
-	}
+    function __construct(RecommendationRepository $repository)
+    {
+        parent::__construct();
 
-	/**
-	 * Display a listing of the resource.
-	 *
-	 * @return Response
-	 */
-	public function index()
-	{
-		$inputs	 = \Input::only("item_id", "user_id", "session_id", "algo", "widget_id");
-		$widget	 = null;
+        $this->repository     = $repository;
+        $this->operator_types = array(
+            'contain'            => 'Contains',
+            'equal'              => 'Equals',
+            'not_equal'          => 'Not Equals',
+            'is_set'             => 'Is Set',
+            'is_not_set'         => 'Is Not Set',
+            'greater_than'       => 'Greater Than',
+            'greater_than_equal' => 'Greater Than or Equal',
+            'less_than'          => 'Less Than',
+            'less_than_equal'    => 'Less Than or Equal'
+        );
 
-		if (isset($inputs['widget_id']))
-		{
-			$widget = \App\Models\Widget::where("id", $inputs['widget_id'])->where("site_id", $this->site_id)->get()->first();
-			if ($widget && $this->_isAlgoExists($widget->reco_type))
-			{
-				
-			}
-		}
-		else
-			$response = $this->getErrorResponse("inputUnknown", 400, "widget_id");
+        $this->response = array(
+            "error"          => false,
+            "status"         => 200,
+            "message"        => "",
+            "client_message" => ""
+        );
 
+        $this->gui_domain_auth = array(
+//            'appid'  => $this->predictry_server_api_key,
+            'appid'  => "pongo", //hardcoded for temporary
+            'domain' => $this->predictry_server_tenant_id
+        );
 
-		return \Response::json($response, $this->http_status);
-	}
+        Gui::setUri(GUI_RESTAPI_URL);
+        Gui::setCredential(GUI_HTTP_USERNAME, GUI_HTTP_PASSWORD);
+        Gui::setDomainAuth($this->gui_domain_auth['appid'], $this->gui_domain_auth['domain']);
+    }
 
-	//GET EASYREC OTHERUSERSALSOVIEWED
-	function _getEasyRecOtherUsersAlsoViewed($option_data)
-	{
-		$easyrec_url_with_method = EASYREC_RESTAPI_URL . "otherusersalsoviewed";
+    /*     * xx
+     * Display a listing of the resource.
+     *
+     * @return Response
+     */
 
-		$this->curl			 = new \Curl();
-		$response			 = $this->curl->_simple_call("get", $easyrec_url_with_method, $option_data);
-		$recommended_items	 = $this->_extractEasyRecResult(json_decode($response));
-		if ($recommended_items)
-			return $recommended_items;
-		else
-			return false;
-	}
+    public function index()
+    {
+        $input     = Input::only("item_id", "user_id", "session_id", "algo", "widget_id");
+        $response  = $reco_data = array();
 
-	function _getEasyRecRecommendationForUser($option_data)
-	{
-		unset($option_data['itemid']);
+        if (isset($input['widget_id'])) {
 
+            $reco_data = $this->repository->populateRecoData($this->site_id, $input, 5);
 
-		$easyrec_url_with_method = EASYREC_RESTAPI_URL . "recommendationsforuser";
-//		$option_data['actiontype'] ='BUY';
+            if (!isset($reco_data['error'])) {
 
-		$this->curl			 = new \Curl();
-		$response			 = $this->curl->_simple_call("get", $easyrec_url_with_method, $option_data);
-		$recommended_items	 = $this->_extractEasyRecResult(json_decode($response));
-		if ($recommended_items)
-			return $recommended_items;
-		else
-			return false;
-	}
+                //get recommendation
+                $response = $this->repository->getRecommendation($reco_data['algo'], $reco_data);
 
-	function _getLokeInsipiredByYourShoppingTrends($user_id)
-	{
-		$url = $this->_getLokeRecoUrl("inspiredbyyourshoppingtrends");
+                if ($response && isset($response->error)) {
+                    $this->http_status = $response->status;
+                }
+                else {
 
-		$this->curl	 = new \Curl();
-		$response	 = $this->curl->_simple_call("get", $url, array('ii' => 'getUserRecomDesc', 'user' => $user_id, 'start' => 0, 'end' => 10));
-//		$response			 = '{"status":0,"rating":[{"user":"17562","product":"10950","score":"2.3945"},{"user":"17562","product":"11884","score":"2.0835"},{"user":"17562","product":"9192","score":"1.9644"},{"user":"17562","product":"8170","score":"1.941"},{"user":"17562","product":"10202","score":"1.9365"},{"user":"17562","product":"10332","score":"1.8903"}]}';
-		$response	 = json_decode($response);
+                    if ($response->data->items && count($response->data->items) > 0) {
 
-		$recommended_items = array();
+                        $item_ids = $response->data->item_ids;
+                        unset($response->data->item_ids);
 
-		if (isset($response->status) && $response->status === 0)
-		{
-			foreach ($response->rating as $rating)
-			{
-				$item = \App\Models\Item::where("identifier", $rating->product)->get()->first();
-				if ($item && $item->active && $this->_isAllowedBasedOnPropertiesFilter($item))
-				{
-					$item_reco = array(
-						"id"				 => $item->identifier,
-						"alias_id"			 => $item->id,
-						"description"		 => $item->name,
-						"created_at"		 => $item->created_at->toDateTimeString(),
-						"item_properties"	 => $this->_getRecoItemProperties($item)
-					);
+                        //no error found, then we have to create new widget instance
+                        $widget_instance_id = $this->repository->createWidgetInstance($input['widget_id'], $input['session_id']);
+                        //when the widget instance ready, then we record the result
+                        if ($widget_instance_id) {
+                            Event::fire("recommendation.response_received", array($item_ids, $widget_instance_id));
+                        }
 
-					array_push($recommended_items, $item_reco);
-				}
-			}
-		}
+                        $response->data->widget_instance_id = $widget_instance_id;
+                    }
+                }
+            }
+            else {
+                $response = $this->getErrorResponse($reco_data['data'][0], $reco_data['data'][1], $reco_data['data'][2]);
+            }
+        }
+        else
+            $response = $this->getErrorResponse("inputUnknown", 400, "widget_id");
 
-		return $recommended_items;
-	}
-
-	//GET GUI RECOMMENDATION
-
-	function _getGuiOtherItemsViewed($option_data)
-	{
-		
-	}
-
-	function _setWidgetInstance($rec_items, $session_id)
-	{
-		$widget_instance			 = new \App\Models\WidgetInstance();
-		$widget_instance->widget_id	 = $this->widget_id;
-
-		$obj_session				 = \App\Models\Session::where("session", $session_id)->get()->first();
-		$widget_instance->session_id = ($obj_session) ? $obj_session->id : 1;
-		$widget_instance->save();
-
-		if ($widget_instance->id)
-		{
-			//Added widget Instance Metas
-			foreach ($rec_items as $item_id)
-			{
-				$widget_instance_item						 = new \App\Models\WidgetInstanceItem();
-				$widget_instance_item->widget_instance_id	 = $widget_instance->id;
-				$widget_instance_item->item_id				 = $item_id;
-				$widget_instance_item->save();
-			}
-		}
-
-		return $widget_instance->id;
-	}
-
-	function _isAlgoExists($algo)
-	{
-		$available_algos = array(
-			"otherusersalsoviewed", "otherusersalsobought", "itemsratedgoodbyotherusers", "recommendationsforuser", "relateditems", "inspiredbyyourshoppingtrends", //easyrec
-			"otherusersalsoviewed2", "otherusersalsoviewedtogether", "otherusersalsobought2", "otherusersalsoboughttogether" //gui
-		);
-		return in_array($algo, $available_algos) ? $algo : $available_algos[0];
-	}
-
-	function _getEasyrecRecoOption($inputs, $options = false)
-	{
-		$rules = array(
-			"item_id"	 => "required",
-			"user_id"	 => "required",
-			"session_id" => "required",
-			"widget_id"	 => "required"
-		);
-
-		$api_credential['tenantid']	 = \Request::header("X-Predictry-Server-Tenant-ID");
-		$api_credential['apikey']	 = \Request::header("X-Predictry-Server-Api-Key");
-
-		$validator = \Validator::make($inputs, $rules);
-
-		if ($validator->passes())
-		{
-			$easyrec_inputs = array(
-				"itemid"			 => $inputs['item_id'],
-				"userid"			 => $inputs['user_id'],
-				"numberOfResults"	 => $this->number_of_results
-			);
-
-			if ($options)
-				return array_merge($api_credential, $easyrec_inputs, $options);
-			else
-				return array_merge($api_credential, $easyrec_inputs);
-		}
-		else
-			return $validator;
-	}
-
-	function _getLokeRecoUrl($method)
-	{
-
-		$site		 = \App\Models\Site::find($this->site_id);
-		$uriMethod	 = "";
-		switch ($method)
-		{
-			case "inspiredbyyourshoppingtrends":
-				$uriMethod = "getUserRecomDesc";
-				break;
-
-			case "otherusersalsoviewed":
-				$uriMethod = "getSimilar";
-
-			default:
-				break;
-		}
-
-		return $site->tenant_engine_url;
-	}
-
-	function _extractEasyRecResult($response)
-	{
-		$recommended_items = array();
-
-		if (isset($response->recommendeditems) && $response->recommendeditems !== null)
-		{
-			foreach ($response->recommendeditems as $items)
-			{
-				if (!is_object($items))
-				{
-					foreach ($items as $item_result)
-					{
-						$item = \App\Models\Item::where("identifier", $item_result->id)->get()->first();
-
-						if ($item && $item->active && $this->_isAllowedBasedOnPropertiesFilter($item))
-						{
-							$item_reco = array(
-								"id"				 => $item->identifier,
-								"alias_id"			 => $item->id,
-								"description"		 => $item->name,
-								"created_at"		 => $item->created_at->toDateTimeString(),
-								"item_properties"	 => $this->_getRecoItemProperties($item)
-							);
-
-							array_push($recommended_items, $item_reco);
-						}
-					}
-				}
-				else
-				{
-					$item_result = $items;
-					$item		 = \App\Models\Item::where("identifier", $item_result->id)->get()->first();
-					if ($item && ($this->_isAllowedBasedOnPropertiesFilter($item) || $this->widget_id === null))
-					{
-						$item_reco = array(
-							"id"				 => $item->identifier,
-							"description"		 => $item->name,
-							"created_at"		 => $item->created_at->toDateTimeString(),
-							"item_properties"	 => $this->_getRecoItemProperties($item)
-						);
-
-						array_push($recommended_items, $item_reco);
-					}
-				}
-			}
-		}
-		return $recommended_items;
-	}
-
-	function _getRecoItemProperties($item)
-	{
-		$item_properties = array();
-		$item_metas		 = \App\Models\ItemMeta::where("item_id", $item->id)->get();
-		if ($item_metas)
-		{
-			foreach ($item_metas as $item_meta)
-			{
-				if ($item_meta->key === "img_url")
-				{
-					$imageUrl	 = $item_meta->value;
-					if (!(strpos($imageUrl, "http") !== false))
-						$imageUrl	 = 'https:' . $item_meta->value;
-
-					if (!$this->_is_url_exist($imageUrl))
-					{
-						//$item_meta->value	 = 'holder.js/160x180/#EE4054:#FFF';
-						$item_meta->value = "https://s3-ap-southeast-1.amazonaws.com/media.redmart.com/newmedia/460x/coming_soon.jpg";
-					}
-				}
-
-				$item_properties[$item_meta->key] = $item_meta->value;
-			}
-		}
-
-		return $item_properties;
-	}
-
-	function _is_url_exist($url)
-	{
-		$ch		 = curl_init($url);
-		curl_setopt($ch, CURLOPT_NOBODY, true);
-		curl_exec($ch);
-		$code	 = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-
-		if ($code == 200)
-		{
-			$status = true;
-		}
-		else
-		{
-			$status = false;
-		}
-		curl_close($ch);
-		return $status;
-	}
-
-	function _isAllowedBasedOnPropertiesFilter($item)
-	{
-		$widget_filters	 = \App\Models\WidgetFilter::where("widget_id", $this->widget_id)->get()->first();
-		$item_metas		 = \App\Models\ItemMeta::where("item_id", $item->id)->get()->lists("value", "key");
-		$bool			 = true;
-
-		if (!(is_array($item_metas) && count($item_metas) > 0))
-			return false;
-
-		if ($widget_filters && $widget_filters->active === 'activated')
-		{
-			$filter_metas = \App\Models\Filtermeta::where("filter_id", $widget_filters->filter_id)->get();
-			foreach ($filter_metas as $meta)
-			{
-				if (isset($item_metas["{$meta->property}"]) && array_key_exists($meta->operator, $this->operator_types))
-				{
-					$item_property_value = $item_metas["{$meta->property}"];
-
-					switch ($meta->operator)
-					{
-						case "contain":
-							$bool = \Str::contains($meta->value, $item_property_value);
-							break;
-
-						case "equal":
-							$bool = ($meta->value === $item_property_value) ? true : false;
-							break;
-
-						case "not_equal":
-							$bool = ($meta->value !== $item_property_value) ? true : false;
-							break;
-
-						case "greater_than":
-							$bool = (is_numeric($item_property_value)) ? ($item_property_value > $meta->value) : false;
-							break;
-
-						case "greater_than_equal":
-							$bool = (is_numeric($item_property_value)) ? ($item_property_value >= $meta->value) : false;
-							break;
-
-						case "less_than":
-							$bool = (is_numeric($item_property_value)) ? ($item_property_value < $meta->value) : false;
-							break;
-
-						case "less_than_equal":
-							$bool = (is_numeric($item_property_value)) ? ($item_property_value <= $meta->value) : false;
-							break;
-
-						default:
-							$bool = true;
-							break;
-					}
-				}
-
-				if (!$bool)
-					return false;
-			}
-		}
-		return $bool;
-	}
+        return Response::json($response, $this->http_status);
+    }
 
 }
 
 /* End of file Recommendation2Controller.php */
-/* Location: ./application/controllers/Recommendation2Controller.php */
--- app/views/frontend/panels/filters/itemfilter.blade.php
+++ app/views/frontend/panels/filters/itemfilter.blade.php
@@ -1,24 +1,26 @@
 <div class="form-group {{ $var = $errors->first('action_id') }} {{ ($var !== '') ? 'has-error' : '' }}" id="item_funel{{$index_item}}">
 	<div class="row">
+		@if ($index_item <= 1)
 		<label for='score' class="col-sm-2 control-label">
-			@if ($index_item <= 1)
 			Filter
-			@endif
 		</label>
+		@else
+		<div class="col-sm-2 text-right">
+			<a href="javascript:void(0);" class="btn btnRemoveItem btn-danger" onClick="removeItem({{ $index_item }}, 'item_funel');"><i class="fa fa-minus"></i></a>
+		</div>
+		@endif
 		<div class="col-sm-4">
 			<?php echo Form::select('property[]', $properties, null, array("class" => "form-control chosen-select-item", "id" => "action{$index_item}", 'data-placeholder' => "Choose Properties...")); ?>
 		</div>
 		<div class="col-sm-2">
 			<?php echo Form::select('operator_key[]', $operator_types, null, array("class" => "form-control", 'data-placeholder' => "Choose Operators...")); ?>
 		</div>
-		<div class="col-sm-3">
+		<div class="col-sm-2">
+			<?php echo Form::select('type[]', $types, null, array("class" => "form-control", 'data-placeholder' => "Choose Data Type...")); ?>
+		</div>
+		<div class="col-sm-2">
 			<?php echo Form::text('value[]', null, array('class' => 'form-control', 'placeholder' => '', 'id' => "value{$index_item}")); ?>
 		</div>
-		@if ($index_item > 1)
-		<div class="col-sm-1">
-			<a href="javascript:void(0);" class="btn btnRemoveItem btn-danger" onClick="removeItem({{ $index_item }}, 'item_funel');"><i class="fa fa-minus"></i></a>
-		</div>
-		@endif	
 	</div>
 </div>
 <?php echo Form::hidden("filter_meta_id[]", -1); ?>
