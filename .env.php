6305b262cf489caac22a67cae95700a57cd5adfd
mohammadhamzehloui <mohammad.hamzehloui@gmail.com>
2014-09-09 13:15:05 +0800

    adding the .env.php file for pongo

---
+++ .env.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+
+    'ENCRYPTION_KEY' => 'GKYGrwJKgQ6NqcJ7elynxXzSN2Q2pbi9',
+    'PGSQL_HOST' => 'predictry.cjrj2aiwv3om.ap-southeast-1.rds.amazonaws.com',
+    'PGSQL_DATABASE' => 'predictry',
+    'PGSQL_USERNAME' => 'predictry',
+    'PGSQL_PASSWORD' => 'MapoKgqQ2rbDecmIzSF5iqjqMsnX4Bwn',
+        "URL"          => "http://dashboard-aws.predictry.com",
+        "DEBUG_MODE"    => false
+);
+
