bd4ee35ce8d697a56683ebe769029108e4f0f8ab
mohammadhamzehloui <mohammad.hamzehloui@gmail.com>
2014-09-11 17:00:32 +0800

    replacing config files and changing the branch to production

--- run.sh
+++ run.sh
@@ -1,12 +1,16 @@
 #! /bin/bash
 
-CWD=$(pwd)
 
 #installing git if it does not exist
  type git  >/dev/null 2>&1 || {
          sudo apt-get install -y git
          echo "git installed successfully"
     }
+
+#cloning required files from git to the image, files such as the nginx configurations, php-fpm and etc... .
+
+sudo git clone https://a0731e397121fcfcd8446931d0dc092da97e0669@github.com/mohammadhamzehloui/aws.git /home/ubuntu/aws
+
 
 #installing php-fpm and php-curl-extension curl if it does not exist
  type php  >/dev/null 2>&1 || {
@@ -48,17 +52,31 @@ fi
 #creating a folder in /usr/share/nginx/html and cloning predictry from github
 
 if [ ! -e "/usr/share/nginx/html/www" ] ; then
-    cd /usr/share/nginx/html && sudo mkdir -p "www/pongo" && cd www/pongo  && sudo git clone https://a0731e397121fcfcd8446931d0dc092da97e0669@github.com/perfectsen/predictry-pongo .
+    cd /usr/share/nginx/html && sudo mkdir -p "www/pongo" && cd www/pongo  && sudo git clone -b production  https://a0731e397121fcfcd8446931d0dc092da97e0669@github.com/predictry/pongo .
 
 fi
 
-#replacing the nginx default file
+#removing the .env.sample.php file from pongo and replacing the one from github 
+sudo rm /usr/share/nginx/html/www/pongo/.env.sample.php 
+sudo cp /home/ubuntu/aws/.env.php /usr/share/nginx/html/www/pongo/
 
-cp $CWD/default /etc/nginx/sites-available/
 
-#replacing php.ini with the default one 
- 
-cp $CWD/php.ini /etc/php5/fpm/php.ini
+#removing the configuration files for nginx php php-fpm  and replacing with the ones from github
+sudo cp /home/ubuntu/aws/default /etc/nginx/sites-available/
+sudo cp /home/ubuntu/aws/nginx.conf /etc/nginx/
+sudo cp /home/ubuntu/aws/php.ini /etc/php5/fpm/
+sudo cp /home/ubuntu/aws/www.conf /etc/php5/fpm/pool.d
+
+#removing the aws folder sudo rm -R /home/ubuntu/aws
+sudo rm -R /home/ubuntu/aws
+
+#running composer 
+#cd /usr/share/nginx/html/www/pongo/ && sudo composer install && sudo php artisan migrate && sudo composer dumpautoload
+cd /usr/share/nginx/html/www/pongo/ && sudo composer install && sudo composer dumpautoload
+
+#changing permission for app/storage
+sudo chmod -R 777 /usr/share/nginx/html/www/pongo/app/storage 
+
 
 
 # reloading nginx and php
