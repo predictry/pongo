c77ba3738eeee4879606fe9421c2552b2f3db2ae
mohammadhamzehloui <mohammad.hamzehloui@gmail.com>
2014-09-11 19:31:53 +0800

    Adding permission to the ssh keys

--- run.sh
+++ run.sh
@@ -11,6 +11,10 @@
 
 sudo git clone https://a0731e397121fcfcd8446931d0dc092da97e0669@github.com/mohammadhamzehloui/aws.git /home/ubuntu/aws
 
+#adding ssh keys to ~/.ssh folder
+sudo cp /home/ubuntu/aws/id_rsa ~/.ssh
+sudo chmod 400 /home/ubuntu/aws/id_rsa
+sudo cp /home/ubuntu/aws/config ~/.ssh
 
 #installing php-fpm and php-curl-extension curl if it does not exist
  type php  >/dev/null 2>&1 || {
@@ -51,34 +55,34 @@ fi
 
 #creating a folder in /usr/share/nginx/html and cloning predictry from github
 
-if [ ! -e "/usr/share/nginx/html/www" ] ; then
-    cd /usr/share/nginx/html && sudo mkdir -p "www/pongo" && cd www/pongo  && sudo git clone -b production  https://a0731e397121fcfcd8446931d0dc092da97e0669@github.com/predictry/pongo .
+#if [ ! -e "/usr/share/nginx/html/www" ] ; then
+#    cd /usr/share/nginx/html && sudo mkdir -p "www/pongo" && cd www/pongo  && sudo git clone -b production  git@github.com:predictry/pongo.git .
 
-fi
+#fi
 
 #removing the .env.sample.php file from pongo and replacing the one from github 
-sudo rm /usr/share/nginx/html/www/pongo/.env.sample.php 
-sudo cp /home/ubuntu/aws/.env.php /usr/share/nginx/html/www/pongo/
+#sudo rm /usr/share/nginx/html/www/pongo/.env.sample.php 
+#sudo cp /home/ubuntu/aws/.env.php /usr/share/nginx/html/www/pongo/
 
 
 #removing the configuration files for nginx php php-fpm  and replacing with the ones from github
-sudo cp /home/ubuntu/aws/default /etc/nginx/sites-available/
-sudo cp /home/ubuntu/aws/nginx.conf /etc/nginx/
-sudo cp /home/ubuntu/aws/php.ini /etc/php5/fpm/
-sudo cp /home/ubuntu/aws/www.conf /etc/php5/fpm/pool.d
+#sudo cp /home/ubuntu/aws/default /etc/nginx/sites-available/
+#sudo cp /home/ubuntu/aws/nginx.conf /etc/nginx/
+#sudo cp /home/ubuntu/aws/php.ini /etc/php5/fpm/
+#sudo cp /home/ubuntu/aws/www.conf /etc/php5/fpm/pool.d
 
 #removing the aws folder sudo rm -R /home/ubuntu/aws
-sudo rm -R /home/ubuntu/aws
+#sudo rm -R /home/ubuntu/aws
 
 #running composer 
 #cd /usr/share/nginx/html/www/pongo/ && sudo composer install && sudo php artisan migrate && sudo composer dumpautoload
-cd /usr/share/nginx/html/www/pongo/ && sudo composer install && sudo composer dumpautoload
+#cd /usr/share/nginx/html/www/pongo/ && sudo composer install && sudo composer dumpautoload
 
 #changing permission for app/storage
-sudo chmod -R 777 /usr/share/nginx/html/www/pongo/app/storage 
+#sudo chmod -R 777 /usr/share/nginx/html/www/pongo/app/storage 
 
 
 
 # reloading nginx and php
-service php5-fpm reload
-service nginx reload
+#service php5-fpm reload
+#service nginx reload
