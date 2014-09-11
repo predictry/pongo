f302ebeb29a8e67cacb0280b5e4aeea0eb01fbdc
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-09-11 11:33:43 +0800

    Feature: Filterting configuration for Recommendation Widget

--- app/controllers/user/ItemsController.php
+++ app/controllers/user/ItemsController.php
@@ -115,6 +115,11 @@ class ItemsController extends \App\Controllers\BaseController
             //something wrong
         }
 
+        $is_dropdown = (!is_null(\Input::get("isDropDown"))) ? \Input::get("isDropDown") : false;
+        $is_view     = (!is_null(\Input::get("isView"))) ? \Input::get("isView") : false;
+        $id          = (!is_null(\Input::get("id"))) ? \Input::get("id") : 1;
+        $value       = (!is_null(\Input::get("val"))) ? \Input::get("val") : false;
+
         $items          = \App\Models\Item::with("item_metas")->where("site_id", $this->active_site_id)->get()->toArray();
         $key_values     = [];
         $key_properties = [];
@@ -129,7 +134,7 @@ class ItemsController extends \App\Controllers\BaseController
         }
 
         //@todo extract the locations to be unique value refer to CODE_TESTING/index2.php
-        $data = [];
+        $data       = $data_group = [];
         foreach ($key_values as $key_value) {
             $arr = (array) $key_value;
 
@@ -139,14 +144,30 @@ class ItemsController extends \App\Controllers\BaseController
 
                 if (key_exists($property, $arr)) {
                     $data[$property] = array_unique(array_merge($data[$property], [ucwords($arr[$property])]));
+
+                    foreach ($data[$property] as $index => $val) {
+                        $data_group[$property][strtolower($property . '.' . \Str::slug($val))] = ucwords($val);
+                    }
                 }
             }
+        }
+
+        if ($is_dropdown) {
+            $key_properties_list = [];
+            foreach ($key_properties as $val) {
+                $key_properties_list[strtolower($val)] = ucwords($val);
+            }
+//            $view = \Form::select("propertyKey[]", $key_properties_list, ($value && $value !== "") ? $value[2] : head($key_properties_list), array('id' => 'propertyKey' . $id, 'class' => 'form-control chosen-select', 'onchange' => 'viewMetaDetail(' . $id . ',"' . $key . '");'));
+            $view = \Form::select("value[]", $data_group, ($value && $value !== "") ? $value : head($key_properties_list), array('id' => 'value' . $id, 'class' => 'form-control chosen-select'));
         }
 
         return \Response::json(array(
                     "status"         => "success",
                     "key_properties" => $key_properties,
-                    "data"           => $data
+                    "data"           => $data,
+                    "is_dropdown"    => $is_dropdown,
+                    "view"           => $view,
+                    "value"          => $value
         ));
     }
 
