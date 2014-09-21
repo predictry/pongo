5545341b29d58517d50c48b1ccc5aa7f467712a9
mohammadhamzehloui <mohammad.hamzehloui@gmail.com>
2014-09-21 08:56:42 +0800

    Removing termination of autoscaling from the bash command, sine we are not using any scheduled actions or rolling policies

--- final.sh
+++ final.sh
@@ -8,34 +8,16 @@ ami=ami-06183c54
 
 if [ ! -z "$ami" ]; then
 
-	aws autoscaling describe-tags >autoscaling.txt
-	auto_scaling=$(grep -m 1 -E -o 'update-pongo[^"]+' autoscaling.txt)
-	sudo rm autoscaling.txt
 
-	if [ ! -z "$auto_scaling" ]; then
-
-		#terminating the process
-		if aws autoscaling  suspend-processes --auto-scaling-group-name $auto_scaling --scaling-processes Terminate ;then
-
-			#updating the template
-			if aws cloudformation update-stack --stack-name update-pongo --template-body file://cloud.json --parameters  ParameterKey=Instanceid,ParameterValue=$ami;then
+	#updating the template
+	if aws cloudformation update-stack --stack-name update-pongo --template-body file://cloud.json --parameters  ParameterKey=Instanceid,ParameterValue=$ami;then
 			
-			echo "The process finished successfully"
-			else
-               		echo "Updating the template failed"
-                	return 1
-                	fi
-
-		else
-		echo "Terminationg autoscaling failed"
-                return 1	
-		fi
-
-
+		echo "The process finished successfully"
 	else
-	echo "can't find the autoscaling name"
-	return 1
-	fi
+        	echo "Updating the template failed"
+        return 1
+
+        fi
 
 else
 echo "There is something wrong with packer"
