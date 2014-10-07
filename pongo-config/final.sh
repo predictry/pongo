e3193c202c78a387d2f8eb4637827890dd06c29a
mohammadhamzehloui <mohammad.hamzehloui@gmail.com>
2014-10-07 22:51:52 +0800

    Fixing the aws credential code in final

--- pongo-config/final.sh
+++ pongo-config/final.sh
@@ -1,10 +1,9 @@
 #! /bin/bash
 
 
-packer build EC2_MAIN_SERVER.json  >image_id.txt
+packer build  -var $aws_access_key -var $aws_secret_key EC2_MAIN_SERVER.json  >image_id.txt
 ami=$(tail -n 1 image_id.txt | grep -E -o 'ami-.{8}')
-sudo rm image_id.txt
-
+-rm $WORKSPACE/pongo-config/image_id.txt
 
 if [ ! -z "$ami" ]; then
 	
