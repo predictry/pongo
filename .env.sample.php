bd4c6cf82477538c19bc83908f080ba20a8c95ff
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-10-30 12:30:01 +0800

    add env values of smtp credentials

--- .env.sample.php
+++ .env.sample.php
@@ -1,12 +1,17 @@
 <?php
 
 return array(
-
-    'ENCRYPTION_KEY' => 'super-secret-sauce',
-    'PGSQL_HOST' => 'localhost',
-    'PGSQL_DATABASE' => 'predictry_db',
-    'PGSQL_USERNAME' => 'predictry_user',
-    'PGSQL_PASSWORD' => 'predictry_password',
-	"URL"		=> "http://dashboard.predictry.dev",
-	"DEBUG_MODE"	=> false
+    'ENCRYPTION_KEY'           => 'super-secret-sauce',
+    'PGSQL_HOST'               => 'localhost',
+    'PGSQL_DATABASE'           => 'predictry_db',
+    'PGSQL_USERNAME'           => 'predictry_user',
+    'PGSQL_PASSWORD'           => 'predictry_password',
+    "URL"                      => "http://dashboard.predictry.dev",
+    "SMTP_HOST"                => 'smtp.host.com',
+    "SMTP_PORT"                => 587,
+    "SMTP_USERNAME"            => 'smtpusername',
+    "SMTP_PASSWORD"            => 'smtppassword',
+    "SMTP_GLOBAL_EMAIL_SENDER" => 'no-reply@predictry.com',
+    "SMTP_GLOBAL_EMAIL_NAME"   => 'Recommendation engine powered by Predictry',
+    "DEBUG_MODE"               => false
 );
