019061e91fcc1c17441c30d0b6663f320ff2ea9e
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-10-30 12:06:55 +0800

    remove script.js from being load as a global script to only dashboard layout

--- app/views/frontend/partials/footer.blade.php
+++ app/views/frontend/partials/footer.blade.php
@@ -1,6 +1,5 @@
 </div>
-<p class="text-center">&copy; 2014 Predictry. A <a href='http://www.vventures.asia'>V Ventures</a> company. <br/> <span class="small cl-fade"> Made with <i class="fa fa-heart-o"></i> in KL</span></p>
-<p class="text-center"><small class="text-muted">Version 0.2.4</small></p>
+<p class="text-center pt40">&copy; 2014 Predictry. A <a href='http://www.vventures.asia'>V Ventures</a> company. <br/> <span class="small cl-fade"> Made with <i class="fa fa-heart-o"></i> in KL. <br/> Version 0.2.4.</span></p>
 </div>
 {{ HTML::script('assets/js/jquery-1.11.0.min.js') }}
 {{ HTML::script('assets/js/bootstrap.min.js') }}
@@ -10,7 +9,6 @@
 {{ $script or 'default' }}
 @endforeach
 @endif
-{{ HTML::script('assets/js/script.js') }}
 {{ $custom_script or 'default' }}
 </body>
 </html>
