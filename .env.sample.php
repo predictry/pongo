3a09c2f684949e703832a6b5554e77f7c4cf92e3
Rifki Yandhi <rifkiyandhi@gmail.com>
2014-11-19 14:08:21 +0800

    add sqs config

--- .env.sample.php
+++ .env.sample.php
@@ -1,12 +1,15 @@
 <?php
 
 return array(
-
-    'ENCRYPTION_KEY' => 'super-secret-sauce',
-    'PGSQL_HOST' => 'localhost',
-    'PGSQL_DATABASE' => 'predictry_db',
-    'PGSQL_USERNAME' => 'predictry_user',
-    'PGSQL_PASSWORD' => 'predictry_password',
-	"URL"		=> "http://dashboard.predictry.dev",
-	"DEBUG_MODE"	=> false
+    'ENCRYPTION_KEY'   => 'super-secret-sauce',
+    'PGSQL_HOST'       => 'localhost',
+    'PGSQL_DATABASE'   => 'predictry_db',
+    'PGSQL_USERNAME'   => 'predictry_user',
+    'PGSQL_PASSWORD'   => 'predictry_password',
+    "URL"              => "http://dashboard.predictry.dev",
+    "DEBUG_MODE"       => false,
+    "SQS_QUEUE_KEY"    => "aws_key",
+    "SQS_QUEUE_SECRET" => "aws_secret",
+    "SQS_QUEUE_URL"    => "aws_url",
+    "SQS_QUEUE_REGION" => "aws_region"
 );
