da0b91250e2459567b7414f5a40b1aaa35f73555
Rifki Yandhi <rifkiyandhi@gmail.com>
2015-01-13 18:07:01 +0800

    fix extend ordering

--- app/views/frontend/themes/inspinia/layouts/dashboard.blade.php
+++ app/views/frontend/themes/inspinia/layouts/dashboard.blade.php
@@ -1,5 +1,5 @@
+@include(getenv('FRONTEND_SKINS') . $theme . '.partials.header', array('styles' => array(HTML::style('assets/css/daterangepicker-bs3.css'), HTML::style('assets/css/bootstrap-datetimepicker.min.css'))))
 <div id="wrapper">
-    @include(getenv('FRONTEND_SKINS') . $theme . '.partials.header', array('styles' => array(HTML::style('assets/css/daterangepicker-bs3.css'), HTML::style('assets/css/bootstrap-datetimepicker.min.css'))))
     @if(Session::get("active_site_id") !== null)
     @include(getenv('FRONTEND_SKINS') . $theme . '.partials.panelsidebar')
     @endif
@@ -12,4 +12,4 @@
     </div>
 
 </div>
-@include(getenv('FRONTEND_SKINS') . $theme . '.partials.footer', array('custom_script' => $custom_script, 'scripts' => array(HTML::script('assets/js/chosen-1.1.0/chosen.jquery.min.js'), HTML::script('assets/js/moment.min.js'), HTML::script('assets/js/daterangepicker.js'), HTML::script('assets/js/highcharts.js'), HTML::script('assets/js/bootstrap-datetimepicker.min.js'), HTML::script('assets/js/script.panel.helper.js'), HTML::script('assets/js/script.panel.filters.js')))))
+@include(getenv('FRONTEND_SKINS') . $theme . '.partials.footer', array('custom_script' => $custom_script, 'scripts' => array(HTML::script('assets/js/chosen-1.1.0/chosen.jquery.min.js'), HTML::script('assets/js/moment.min.js'), HTML::script('assets/js/daterangepicker.js'), HTML::script('assets/js/highcharts.js'), HTML::script('assets/js/bootstrap-datetimepicker.min.js'), HTML::script('assets/js/script.helper.js'), HTML::script('assets/js/script.panel.filters.js')))))
