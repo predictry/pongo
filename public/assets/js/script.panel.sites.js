9d2ef0b6b12306032f70a3f461707d2bfde658c6
Rifki Yandhi <rifkiyandhi@gmail.com>
2015-01-28 17:34:05 +0800

    script for site panel

---
+++ public/assets/js/script.panel.sites.js
@@ -0,0 +1,25 @@
+jQuery(document).ready(function () {
+
+    var navListItems = $('ul.setup-panel li a'),
+            allWells = $('.setup-content');
+
+    allWells.hide();
+
+    navListItems.click(function (e) {
+        e.preventDefault();
+        var $target = $($(this).attr('href')),
+                $item = $(this).closest('li');
+
+        if (!$item.hasClass('disabled')) {
+            navListItems.closest('li').removeClass('active');
+            $item.addClass('active');
+            allWells.hide();
+            $target.show();
+        }
+    });
+
+    $('ul.setup-panel li.active a').trigger('click');
+
+    $('.navbar-minimalize').trigger('click');
+
+});
