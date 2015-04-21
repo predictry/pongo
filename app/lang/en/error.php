d996ee7917a0a081eb2265d25553553a79683d1c
Rifki Yandhi <rifkiyandhi@gmail.com>
2015-04-21 13:39:57 +0800

    add error lang

---
+++ app/lang/en/error.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Author       : Rifki Yandhi
+ * Date Created : Apr 21, 2015 11:52:10 AM
+ * File         : error.php
+ * Function     : 
+ */
+return array(
+    "email.doesnt.exists"        => "Email doesn't exists.",
+    "login.failed"               => "Your email/password combination was incorrect.",
+    "email.doesnt.exists"        => "Email you have entered doesn't exists.",
+    "account.have.not.confirmed" => "Account have not confirmed yet. Please check the email that we have send to you earlier.",
+);
