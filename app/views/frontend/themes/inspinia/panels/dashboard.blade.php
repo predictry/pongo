56296d41b2fe0e91f94b1010f801a0d2a240fd7d
talnet <tinaunglinn.eng91@gmail.com>
2015-06-15 15:41:33 +0800

    Enable top(5) of viewed && pruchased

--- app/views/frontend/themes/inspinia/panels/dashboard.blade.php
+++ app/views/frontend/themes/inspinia/panels/dashboard.blade.php
@@ -1,5 +1,14 @@
-@extends(getenv('FRONTEND_SKINS') . $theme . '.layouts.dashboard', ['scripts' => array(HTML::script('assets/js/chosen-1.1.0/chosen.jquery.min.js'), HTML::script('assets/js/moment.min.js'), HTML::script('assets/js/daterangepicker.js'), HTML::script('assets/js/highcharts.js'), HTML::script('assets/js/bootstrap-datetimepicker.min.js'), HTML::script('assets/js/script.helper.js'), HTML::script('assets/js/script.panel.filters.js'))])
+@extends(getenv('FRONTEND_SKINS') . $theme . '.layouts.dashboard', ['scripts' => array(
+HTML::script('assets/js/chosen-1.1.0/chosen.jquery.min.js'), 
+HTML::script('assets/js/moment.min.js'), 
+HTML::script('assets/js/daterangepicker.js'), 
+HTML::script('assets/js/highcharts.js'), 
+HTML::script('assets/js/bootstrap-datetimepicker.min.js'), 
+HTML::script('assets/js/script.helper.js'), 
+HTML::script('assets/js/script.panel.filters.js'))])
+
 @section('content')
+
 <div class="wrapper wrapper-content">
     <div class="ibox float-e-margins">
         <div class="ibox-title">
@@ -12,11 +21,12 @@
     @include(getenv('FRONTEND_SKINS') . $theme . '.panels.overview_summary')
     <div class="row">
         <div class="col-lg-6">
-            {{--@include(getenv('FRONTEND_SKINS') . $theme . '.panels.top_5', ['tableHeader' => 'Top 10 (Most Purchased Items)', 'contents' => $top_purchased_items])--}}
+          @include(getenv('FRONTEND_SKINS') . $theme . '.panels.top_5', ['tableHeader' => 'Top 10 (Most Purchased Items)', 'contents' => $top_purchased_items])
         </div>
         <div class="col-lg-6">
-            {{--@include(getenv('FRONTEND_SKINS') . $theme . '.panels.top_5', ['tableHeader' => 'Top 10 (Most Viewed Items)', 'contents' => $top_viewed_items])--}}
+            @include(getenv('FRONTEND_SKINS') . $theme . '.panels.top_5', ['tableHeader' => 'Top 10 (Most Viewed Items)', 'contents' => $top_viewed_items])
         </div>
     </div>
 </div>
+
 @stop
