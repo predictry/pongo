c434c08e7d88f50f5fe3e0a3951d1606ecf6f92e
jocki <jocki.predictry@gmail.com>
2016-03-07 18:28:16 +0800

    Replace summernote rich text editor with Ace code editor so user can customize HTML and CSS design.

--- app/views/frontend/themes/inspinia/panels/email/emailtargeting.blade.php
+++ app/views/frontend/themes/inspinia/panels/email/emailtargeting.blade.php
@@ -12,10 +12,8 @@
         HTML::script('assets/js/script.helper.js'),
         HTML::script('assets/js/script.panel.filters.js'),
         HTML::script('assets/js/visual.js'),
-        HTML::script('assets/js/summernote.js')
-    ],
-    'styles' => [
-         HTML::style('assets/css/summernote.css')
+        HTML::script('assets/js/ace/ace.js'),
+        HTML::script('assets/js/ace/ext-beautify.js')
     ]
 ])
 @section('content')
@@ -102,11 +100,7 @@
                                         </button>
                                         <ul class="dropdown-menu">
                                             <li>
-                                                <a onclick="rBlocks()">Recommended For You ( 4 )</a>
-                                            </li>
-                                            <li class="divider"></li>
-                                            <li>
-                                                <a onclick="rBlocks()">Recommended For You ( 8 )</a>
+                                                <a id="insert_rec_4">Recommended For You ( 4 )</a>
                                             </li>
                                         </ul>
                                     </div>
@@ -117,63 +111,100 @@
                 </div>
             </div>
         </div>
-    <div id="summernote"></div>
-    <div class="row">
-        <div class="col-md-12">
-            <div class="pull-right">
-                <button type="button" class="btn btn-warning" id="sdraft">Save as Draft</button>
-                <button type="button" class="btn btn-primary" id="semail" onclick="sendEmail()">Send Email</button>
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="tabs-container">
+                    <ul class="nav nav-tabs">
+                        <li class="active"><a data-toggle="tab" href="#email-html">HTML</a></li>
+                        <li><a data-toggle="tab" href="#email-preview">Preview</a></li>
+                    </ul>
+                    <div class="tab-content">
+                        <div id="email-html" class="tab-pane active">
+                            <div class="panel-body">
+                                <div id="editor" style="width: 100%; height: 300px; margin-bottom: 10px;"></div>
+                            </div>
+                        </div>
+                        <div id="email-preview" class="tab-pane">
+                            <div class="panel-body">
+                                <iframe id="email-preview-frame" style="width: 100%; height: 300px; background-color: white; border: 0px;"></iframe>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
-    <script>
 
-        function rBlocks() {
-            var node = document.createElement('div');
-            var t = document.createTextNode("Block of 8");
-            node.appendChild(t)
-            node.style.cssText = "width:100%;height:100px;background:grey;color: #FFF;text-align: center; margin-top: 20px; margin-left: 5px;line-height: 90px;";
+        <div class="row">
+            <div class="col-md-12">
+                <div class="pull-right">
+                    <button type="button" class="btn btn-warning" id="sdraft">Save as Draft</button>
+                    <button type="button" class="btn btn-primary" id="send_email">Send Email</button>
+                </div>
+            </div>
+        </div>
 
+        <script>
+            $(document).ready(function() {
 
-            $('#summernote').summernote('insertNode', node);
+                var defaultMailMessage = '<!DOCTYPE html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n</head>\n<body>\n\t<h1>Replace your message here!</h1>\n</body>\n</html>';
+                var previewFrameHtml = $('#email-preview-frame').contents().find('html');
+                var editor = ace.edit("editor");
+                editor.setTheme("ace/theme/chrome");
+                editor.getSession().setMode("ace/mode/html");
+                editor.setValue(defaultMailMessage);
+                editor.getSession().on('change', function() {
+                    previewFrameHtml.html(editor.getSession().getValue());
+                });
+                previewFrameHtml.html(defaultMailMessage);
 
-        }
+                $("#insert_rec_4").click(function() {
+                    editor.insert(
+                        '<div id="duo4">\n' +
+                        '\t<div>\n' +
+                        '\t\t<h3>You Bought</h3>\n' +
+                        '\t\t<div>\n' +
+                        '\t\t\t<a th:href="${products[0].item_url}"><img th:src="${products[0].img_url}" src="https://placehold.it/350x150"/></a>\n' +
+                        '\t\t\t<div><a th:href="${products[0].item_url}"><span th:text="${products[0].name}">Item1</span></a></div>\n' +
+                        '\t\t\t<div>RM <span th:text="${products[0].price}">123</span></div>\n' +
+                        '\t\t</div>\n' +
+                        '\t</div>\n' +
+                        '\t<div class="recommendations">\n' +
+                        '\t\t<h3>Other People Also Bought</h3>\n' +
+                        '\t\t<ul>\n' +
+                        '\t\t\t<li th:each="rec : ${products[0].recs}">\n' +
+                        '\t\t\t\t<div>\n' +
+                        '\t\t\t\t\t<a th:href="${rec.item_url}"><img th:src="${rec.img_url}" src="https://placehold.it/350x150"/></a>\n' +
+                        '\t\t\t\t\t<div><a th:href="${rec.item_url}"><span th:text="${rec.name}">Item2</span></a></div>\n' +
+                        '\t\t\t\t\t<div>RM <span th:text="${rec.price}">123</span></div>\n' +
+                        '\t\t\t\t</div>\n' +
+                        '\t\t\t</li>\n' +
+                        '\t\t</ul>\n' +
+                        '\t</div>\n' +
+                        '</div>');
+                });
 
-        function sendEmail(){
-            var data = {
-                "pongoUserId": {{ Auth::user()->id }},
-                "campaignName": document.getElementById("campaignname").value,
-                "targets": [{"action": "BUY", "day": document.getElementById("timeframe").value}],
-                "mandrillAPIKey": document.getElementById("apikey").value,
-                "emailFrom": document.getElementById("usersname").value,
-                "emailSubject": document.getElementById("subject").value,
-                "template": $('#summernote').summernote('code')
-
-            };
-
-            $.post( "fisher.predictry.com:8090/oms", function( data ) {
-                if(data.status == 'created'){
-                    toastr.success('Your Email Campaign has Started','Success!');
-                }else{
-                    toastr.warning('Your Email Campaign has failed to Start','Error');
-                }
+                $("#send_email").click(function() {
+                    var data = {
+                        "pongoUserId": {{ Auth::user()->id }},
+                        "campaignName": document.getElementById("campaignname").value,
+                        "targets": [{"action": "BUY", "day": document.getElementById("timeframe").value}],
+                        "mandrillAPIKey": document.getElementById("apikey").value,
+                        "emailFrom": document.getElementById("usersname").value,
+                        "emailSubject": document.getElementById("subject").value,
+                        "template": editor.getValue()
+                    };
+                    $.post("fisher.predictry.com:8090/oms", function (data) {
+                        if (data.status == 'created') {
+                            toastr.success('Your Email Campaign has Started', 'Success!');
+                        } else {
+                            toastr.warning('Your Email Campaign has failed to Start', 'Error');
+                        }
+                    });
+                });
             });
-            console.log("data is" + JSON.stringify(data));
-        }
-        document.onreadystatechange = function () {
-            $('#summernote').summernote({
-                toolbar: [
-                    ['style', ['bold', 'italic', 'underline']],
-                    ['font', ['strikethrough', 'superscript', 'subscript']],
-                    ['fontsize', ['fontsize']],
-                    ['color', ['color']],
-                    ['para', ['ul', 'ol', 'paragraph']],
-                    ['height', ['height']],
-                    ['mybutton', ['recblocks']]
-                ]
-            });
-        }
-    </script>
+
+        </script>
     </div>
 
 @stop
